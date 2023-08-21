f485caab06f4bbe27dd74b6ec1ba13645959ea9f
damneets <damneets59@gmail.com>
2023-08-21 20:16:07 +0530

    tried for too long

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,7 @@
 gender=input("whats ur gender?")
 if gender=="male":
     print("okay lil bro")
-else:
+elif gender=="female":
     print("wasgud shawty")
+else:
+    print("youre not real, cry bout it")
